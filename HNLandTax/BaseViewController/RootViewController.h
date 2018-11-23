9be1c1e7af3ba8c4abc30b99aa1d9a3ce873bd47
wzz <xzzhenw@163.com>
2018-11-23 18:20:53 +0800

    no message

--- HNLandTax/BaseViewController/RootViewController.h
+++ HNLandTax/BaseViewController/RootViewController.h
@@ -7,54 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "UIViewController+BarButtonItem.h"
 @interface RootViewController : UIViewController
 @property (nonatomic, assign) BOOL banSlidingBack;
 
 /**
-是否隐藏nav下的白线
- */
--(BOOL)jm_hiddenNavShadowImage;
-
-
-/**
- 通过图片创建导航栏右边按钮，需重写ld_rightBarButtonItemAction:
- @param imageName 图片名称
- */
-- (void)jm_createLeftBarButtonItemWithImage:(NSString *)imageName;
-/**
- 通过文字创建导航栏右边按钮,需重写ld_rightBarButtonItemAction:
- @param title 按钮名称
- */
-- (void)jm_createLeftBarButtonItemWithTitle:(NSString *)title;
-/**
- 导航右边按钮的点击行为
- */
-- (void)jm_leftBarButtonItemAction:(UIBarButtonItem *)barButtonItem;
-
-
-
-/**
- 通过图片创建导航栏右边按钮，需重写ld_rightBarButtonItemAction:
- @param imageName 图片名称
- */
-- (void)jm_createRightBarButtonItemWithImage:(NSString *)imageName;
-
-/**
- 通过文字创建导航栏右边按钮,需重写ld_rightBarButtonItemAction:
- @param title 按钮名称
- */
-- (void)jm_createRightBarButtonItemWithTitle:(NSString *)title;
-
-/**
- 导航右边按钮的点击行为
- */
-- (void)jm_rightBarButtonItemAction:(UIBarButtonItem *)barButtonItem;
-
-
-/**
  image handle
-
  @param image 图片
  */
 - (void)cameraOrAlbumObtainPhoto:(UIImage *)image;
--- HNLandTax/BaseViewController/RootViewController.m
+++ HNLandTax/BaseViewController/RootViewController.m
@@ -16,107 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
 }
-
-#pragma mark - 侧滑返回
--(void)viewWillAppear:(BOOL)animated{
-    [super viewWillAppear:animated];
-    self.navigationController.interactivePopGestureRecognizer.delegate = self;
-
-}
-
-- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
-    if (self.banSlidingBack) {
-        return NO;
-    }else{
-        return self.navigationController.viewControllers.count > 1;
-    }
-}
-
-
-#pragma mark - 设置父类导航
--(void)setSuperNavnavigation{
-    self.view.backgroundColor = [UIColor whiteColor];
-    if (self.navigationController.viewControllers.count>1) {
-        self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc]init];
-        self.navigationItem.backBarButtonItem.title = @"返回";
-        //        self.navigationItem.leftItemsSupplementBackButton = YES;
-        //        self.navigationController.navigationBar.backIndicatorImage = [UIImage imageNamed:@"backImageName"];
-        //        self.navigationController.navigationBar.backIndicatorTransitionMaskImage = [UIImage imageNamed:@"backImageName"];
-    }
-    self.navigationController.interactivePopGestureRecognizer.delegate = self;
-//    [self findHiddenShadowImage];
-}
-
-
-
-
-#pragma mark - barButtonItem left && right
-
-- (void)jm_createRightBarButtonItemWithImage:(NSString *)imageName{
-    NSAssert(imageName.length>0,@"图片名称不能为空");
-    self.navigationItem.rightBarButtonItem = [self rightBarButtonItemWithImage:imageName];
-}
-
-- (void)jm_createRightBarButtonItemWithTitle:(NSString *)title{
-    NSAssert(title.length>0,@"title 不能为空");
-    self.navigationItem.rightBarButtonItem = [self rightBarButtonItemWithTitle:title];
-}
-
-- (void)jm_createLeftBarButtonItemWithImage:(NSString *)imageName{
-    NSAssert(imageName.length>0,@"图片名称不能为空");
-    self.navigationItem.leftBarButtonItem = [self leftBarButtonItemWithImage:imageName];
-}
-
-- (void)jm_createLeftBarButtonItemWithTitle:(NSString *)title{
-    NSAssert(title.length>0,@"title 不能为空");
-    self.navigationItem.leftBarButtonItem = [self leftBarButtonItemWithTitle:title];
-}
-
-
-- (UIBarButtonItem *)leftBarButtonItemWithImage:(NSString *)imageName{
-    UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:imageName]
-                                                                      style:UIBarButtonItemStylePlain
-                                                                     target:self
-                                                                     action:@selector(jm_leftBarButtonItemAction:)];
-    return barButtonItem;
-    
-}
-- (UIBarButtonItem *)leftBarButtonItemWithTitle:(NSString *)title{
-    UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithTitle:title
-                                                                      style:UIBarButtonItemStylePlain
-                                                                     target:self
-                                                                     action:@selector(jm_leftBarButtonItemAction:)];
-    return barButtonItem;
-}
-
-- (UIBarButtonItem *)rightBarButtonItemWithImage:(NSString *)imageName{
-    UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:imageName]
-                                                                      style:UIBarButtonItemStylePlain
-                                                                     target:self
-                                                                     action:@selector(jm_rightBarButtonItemAction:)];
-    return barButtonItem;
-    
-}
-
-- (UIBarButtonItem *)rightBarButtonItemWithTitle:(NSString *)title{
-    UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithTitle:title
-                                                                      style:UIBarButtonItemStylePlain
-                                                                     target:self
-                                                                     action:@selector(jm_rightBarButtonItemAction:)];
-    return barButtonItem;
-}
-
-- (void)jm_rightBarButtonItemAction:(UIBarButtonItem *)barButtonItem{
-    
-}
-
-- (void)jm_leftBarButtonItemAction:(UIBarButtonItem *)barButtonItem{
-    [self.navigationController popViewControllerAnimated:YES];
-}
-
-
 
 #pragma mark - image handle
 -(void)cameraOrAlbumObtainPhoto:(UIImage *)image{
@@ -126,28 +26,28 @@
     
 }
 
-#pragma mark - 隐藏导航栏 shadow image
--(BOOL)jm_hiddenNavShadowImage{
-    return YES;
-}
-
--(void)findHiddenShadowImage{
-    UIImageView *iv = [self findShadowImage:self.navigationController.navigationBar];
-    iv.hidden = [self jm_hiddenNavShadowImage];
-}
-
--(UIImageView *)findShadowImage:(UIView *)aView{
-    if ([aView isKindOfClass:[UIImageView class]] && aView.bounds.size.height <= 1) {
-        return (UIImageView *)aView;
-    }
-    for (UIView *view in aView.subviews) {
-        UIImageView *iv =  (UIImageView *)[self findShadowImage:view];
-        if (iv) {
-            return iv;
-        }
-    }
-    return  nil;
-}
+//#pragma mark - 隐藏导航栏 shadow image
+//-(BOOL)jm_hiddenNavShadowImage{
+//    return YES;
+//}
+//
+//-(void)findHiddenShadowImage{
+//    UIImageView *iv = [self findShadowImage:self.navigationController.navigationBar];
+//    iv.hidden = [self jm_hiddenNavShadowImage];
+//}
+//
+//-(UIImageView *)findShadowImage:(UIView *)aView{
+//    if ([aView isKindOfClass:[UIImageView class]] && aView.bounds.size.height <= 1) {
+//        return (UIImageView *)aView;
+//    }
+//    for (UIView *view in aView.subviews) {
+//        UIImageView *iv =  (UIImageView *)[self findShadowImage:view];
+//        if (iv) {
+//            return iv;
+//        }
+//    }
+//    return  nil;
+//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
